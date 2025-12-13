3bfadf584195f5212443dfa61a7e9d242b836921
Andrea <bwamsterdam@gmail.com>
2025-12-13 09:49:33 +0000

    change ship correct

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -1,3 +1,21 @@
+with source as (
+
+    select *
+    from {{ source('raw', 'ship') }}
+
+),
+
+cleaned as (
+
+    select
+        ship_id,
+        shipping_fee,
+        cast(ship_cost as float64) as ship_cost
+    from source
+
+)
+
 select *
-from raw.ship
-where shipping_fee <> shipping_fee_1
+from cleaned
+
+
